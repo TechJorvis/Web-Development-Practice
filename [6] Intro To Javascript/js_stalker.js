df0800cc895eadda425904fdcc7a9f3b6f0c4610
Ayush Singhal <ayush.singhal.as30@gmail.com>
2018-11-06 00:23:06 +0530

    Beginner JS Exercise

---
+++ [6] Intro To Javascript/js_stalker.js
@@ -0,0 +1,11 @@
+// Input the first name of the user
+let firstName = window.prompt("Pleaser enter your first name");
+
+// Input the last name of the user
+let lastName = window.prompt("Please enter your last name");
+
+// Input the age of the user
+let user_age = window.prompt("Please enter your age");
+
+console.log("Hello, " + firstName + " " + lastName);
+console.log("You entered your age as: " + user_age);
