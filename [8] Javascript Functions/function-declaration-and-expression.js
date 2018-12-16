319b6d111832d4b65bea131393499866269e9e08
Ayush Singhal <ayush.singhal.as30@gmail.com>
2018-12-16 22:40:31 +0530

    Javascript Function Declaration vs Function Expression

---
+++ [8] Javascript Functions/function-declaration-and-expression.js
@@ -0,0 +1,15 @@
+// Function declaration
+function capitalize(str) {
+    console.log("Fuction Declaration: " + str);
+    // Function body
+}
+
+// Function expression
+var Capitalize = function(str) {
+    console.log("Function Expression: " + str);
+    // Function body
+}
+
+capitalize("Ayush");
+//console.log(Capitalize);
+Capitalize("Ayush");
