23a9fbe9e0d711844b463dbcab54eaab16a48d09
Ayush Singhal <ayush.singhal.as30@gmail.com>
2018-12-19 00:18:08 +0530

    forEach loop Demo in JS

---
+++ [9] Javascript Arrays/forEach.js
@@ -0,0 +1,9 @@
+/**
+ * For Each loop in JS
+ */
+
+anime_list = ["Boruto", "Dragon Ball", "Fairy Tail", "Naruto", "Sword Art Online"];
+
+anime_list.forEach(function(anime) {
+    console.log("> " + anime);
+});
