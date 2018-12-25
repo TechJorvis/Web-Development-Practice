5d19dd94796381251c4c557311ccbe153aab0d8e
Ayush Singhal <ayush.singhal.as30@gmail.com>
2018-12-26 03:14:59 +0530

    Added logic to generate random colors for the color palette

--- [13] Color Game/color-game.js
+++ [13] Color Game/color-game.js
@@ -1,19 +1,9 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-
 var pickedColor = pickColor();
-
 var colorDisplay = document.getElementById("color-display");
-
 var messageDisplay = document.getElementById("message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor.slice(4, pickedColor.indexOf(')'));
 
@@ -29,6 +19,8 @@ for (var i = 0; i < squares.length; i++) {
             messageDisplay.style.color = "white";
 
             changeColor(clickedColor);
+
+            h1.style.backgroundColor = clickedColor;
         } else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again :(";
@@ -46,4 +38,22 @@ function changeColor(color) {
 function pickColor() {
     let random_index = Math.floor(Math.random() * colors.length);
     return colors[random_index];
+}
+
+function generateRandomColors(colorCount) {
+    let arr = [];
+    for (let i = 0; i < colorCount; i++) {
+        arr.push(randomColor())
+    }
+    return arr;
+}
+
+function randomColor() {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+
+    let rgb = "rgb(" + red + ", " + green + ", " + blue + ")";
+
+    return rgb;
 }
