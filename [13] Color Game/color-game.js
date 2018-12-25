48b3ac77c7212da6784ca41d51fbc67be9167158
Ayush Singhal <ayush.singhal.as30@gmail.com>
2018-12-25 22:57:36 +0530

    Basic Game Built Complete

---
+++ [13] Color Game/color-game.js
@@ -0,0 +1,36 @@
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)"
+];
+
+var squares = document.querySelectorAll(".square");
+
+var pickedColor = colors[3];
+
+var colorDisplay = document.getElementById("color-display");
+
+var messageDisplay = document.getElementById("message");
+
+colorDisplay.textContent = pickedColor.slice(4, pickedColor.indexOf(')'));
+
+for (var i = 0; i < squares.length; i++) {
+    // Add initial colors to squares
+    squares[i].style.backgroundColor = colors[i];
+
+    // Add click listeners to squares
+    squares[i].addEventListener("click", function() {
+        var clickedColor = this.style.backgroundColor;
+        if (clickedColor === pickedColor) {
+            messageDisplay.textContent = "Correct!";
+            messageDisplay.style.color = "white";
+        } else {
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again :(";
+            messageDisplay.style.color = "white";
+        }
+    });
+}
