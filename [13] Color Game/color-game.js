ac75c4a2091d4c081cec4630addb9f16c6de8a5c
Ayush Singhal <ayush.singhal.as30@gmail.com>
2018-12-26 14:20:03 +0530

    Added logic to implement the easy and hard mode

--- [13] Color Game/color-game.js
+++ [13] Color Game/color-game.js
@@ -1,14 +1,47 @@
-var colors = generateRandomColors(6);
+var nbrOfSquares = 6;
+var colors = generateRandomColors(nbrOfSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("color-display");
 var messageDisplay = document.getElementById("message");
 var h1 = document.querySelector("h1");
 var resetButton = document.getElementById("reset");
+var easyButton = document.getElementById("easy-mode");
+var hardButton = document.getElementById("hard-mode");
+
+easyButton.addEventListener("click", function() {
+    hardButton.classList.remove("selected");
+    easyButton.classList.add("selected");
+    nbrOfSquares = 3;
+    colors = generateRandomColors(nbrOfSquares);
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor.slice(4, pickedColor.indexOf(')'));
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardButton.addEventListener("click", function() {
+    easyButton.classList.remove("selected");
+    hardButton.classList.add("selected");
+    nbrOfSquares = 6;
+    colors = generateRandomColors(nbrOfSquares);
+    pickedColor = pickColor()
+    colorDisplay.textContent = pickedColor.slice(4, pickedColor.indexOf(')'));
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
+
 
 resetButton.addEventListener("click", function() {
     // Generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(nbrOfSquares);
     // Pick a new random color from array
     pickedColor = pickColor();
     // Change the colorDisplay to match pickedColor
@@ -18,7 +51,7 @@ resetButton.addEventListener("click", function() {
         squares[i].style.backgroundColor = colors[i];
     }
     // Revert back the background of h1
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "steelBlue";
     // Revert the text of the button to "New Colors"
     resetButton.textContent = "New Colors";
 })
