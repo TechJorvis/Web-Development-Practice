891c90c3a090fc3059f94ce3e8acfd8fdfab8969
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-05 18:56:21 +0530

    First Express App

---
+++ [18] Server Side Frameworks/FirstExpressApp/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var ip = require('ip');
+
+var app = express();
+
+// "/" => "Hi"
+app.get("/", function(req, res) {
+    res.send("Hi There!");
+});
+
+// "/bye" => Goodbye!
+app.get("/bye", function(req, res) {
+    res.send("Goodbye!");
+});
+
+// "/dog" => "MEOW"
+app.get("/dog", function(req, res) {
+    res.send("MEOW!");
+});
+
+// Tell Express to listen for requests (start server)
+app.listen(3000, ip.address, function() {
+    console.log("Server has at: " + ip.address() + ":3000");
+});
