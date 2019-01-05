129d7b5c053177aaec965af8a6fd1a940d732bb0
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-05 15:22:21 +0530

    Echo exercise completed

---
+++ [17] Node JS/echo.js
@@ -0,0 +1,10 @@
+function echo(str, freq) {
+    for (let i = 0; i < freq; i++) {
+        console.log(str);
+    }
+}
+// End of function echo
+
+/* TESTING THE FUNCTION */
+echo("Echo!", 10);
+echo("Yo!", 4);
