ac65809d49c4149c509e1a520079f5c7cf4d239d
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-08 22:20:57 +0530

    Basic server setup completed

---
+++ [19] Intermediate Express/EJS_Loops_Demo/app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var ip = require('ip');
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Welcome to homepage!");
+});
+
+app.get("/posts", function(req, res) {
+    let posts = [
+        {title: "GraphQL", author: "John"},
+        {title: "Java", author: "Johsh"},
+        {title: "NodeJS", author: "Ben"}
+    ];
+});
+
+app.listen(3000, ip.address, function() {
+    console.log("Server started serving at: " + ip.address() + ":3000");
+});
