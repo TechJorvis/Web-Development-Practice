6739ba474e879914aec4a75be93aa1c0986ca175
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-08 09:58:36 +0530

    Basic server setup complete

---
+++ [19] Intermediate Express/EJSDemo/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var ip = require('ip');
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Welcome to the home page!");
+});
+
+app.listen(3000, ip.address, function() {
+    console.log("Server started serving at: " + ip.address() + ":3000");
+});
