d9366635f3e00252365c5d542949d6064607d2aa
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-08 10:57:30 +0530

    First ejs demo completed

--- [19] Intermediate Express/EJSDemo/app.js
+++ [19] Intermediate Express/EJSDemo/app.js
@@ -4,7 +4,13 @@ var ip = require('ip');
 var app = express();
 
 app.get("/", function(req, res) {
-    res.send("Welcome to the home page!");
+    //res.send("Welcome to the home page!");
+    res.render("home.ejs");
+});
+
+app.get("/fallinlovewith/:thing", function(req, res) {
+    var thing = req.params.thing;
+    res.render("love.ejs", {thingVar: thing});
 });
 
 app.listen(3000, ip.address, function() {
