97dd2a6b256e6c8272035b392c9b6241bc05e3e3
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-09 10:41:32 +0530

    EJS PArtials demo completed

--- [19] Intermediate Express/EJS_Partials_Demo/app.js
+++ [19] Intermediate Express/EJS_Partials_Demo/app.js
@@ -11,6 +11,16 @@ app.get("/", function(req, res) {
     res.render("home");
 });
 
+app.get("/posts", function(req, res) {
+    let anime_list = [
+        {name: "boruto", type: "ninja"},
+        {name: "dragon ball", type: "martial arts"},
+        {name: "fairy tail", type: "magic/adventure"}
+    ];
+
+    res.render("posts", {anime_list: anime_list});
+});
+
 app.listen(3000, ip.address, function() {
     console.log("Server started serving at: " + ip.address() + ":3000");
 });
