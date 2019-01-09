a39b4008ad8b801868d13af5bb53060f4be1681f
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-09 09:40:31 +0530

    posts page request handling added

--- [19] Intermediate Express/EJS_Stylesheet_Demo/app.js
+++ [19] Intermediate Express/EJS_Stylesheet_Demo/app.js
@@ -3,8 +3,20 @@ var ip = require('ip');
 
 var app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
     res.send("Welcome to the homepage!");
+});
+
+app.get("/posts", function(req, res) {
+    let anime_list = [
+        {anime: "boruto", type: "ninja"},
+        {anime: "dragon ball", type: "martial arts"},
+        {anime: "fairy tail", type: "magic/adventure"}
+    ];
+
+    res.render("posts", {anime_list: anime_list});
 });
 
 app.listen(3000, ip.address, function() {
