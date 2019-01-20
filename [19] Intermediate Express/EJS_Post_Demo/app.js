0480d4f31e650c0025f22e08e20d38e9d21a5aa9
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-20 10:53:18 +0530

    home page route created

--- [19] Intermediate Express/EJS_Post_Demo/app.js
+++ [19] Intermediate Express/EJS_Post_Demo/app.js
@@ -5,6 +5,10 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
 app.listen(3000, ip.address, function() {
     console.log("Server started serving at: " + ip.address() + ":3000");
 });
