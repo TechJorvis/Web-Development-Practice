422079d5f02e07854355e797e7724e7482029b4e
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-20 11:24:53 +0530

    Post request demo completed

--- [19] Intermediate Express/EJS_Post_Demo/app.js
+++ [19] Intermediate Express/EJS_Post_Demo/app.js
@@ -8,17 +8,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
+var anime = ['boruto', 'dragon ball', 'dragon ball z', 'dragon ball super', 'dragon ball gt'];
+
 app.get("/", function(req, res) {
     res.render("home");
 });
 
 app.post("/addAnime", function(req, res) {
     let newAnime = req.body.newAnime;
-    res.send("You have reached the post route");
+    anime.push(newAnime);
+    res.redirect("/anime");
 });
 
 app.get("/anime", function(req, res) {
-    var anime = ['boruto', 'dragon ball', 'dragon ball z', 'dragon ball super', 'dragon ball gt'];
     res.render("anime", {anime_list: anime});
 });
 
