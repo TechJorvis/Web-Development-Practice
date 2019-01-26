f246e7883b3e37efd5d97b7e18d4e3fabed7a2f8
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-01-26 22:01:25 +0530

    Added logic to extract and save the data given during signup

--- Practice/EJS-POST-Practice/app.js
+++ Practice/EJS-POST-Practice/app.js
@@ -1,12 +1,33 @@
 const express = require('express');
 const ip = require('ip');
+var bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+var users = [];
 
 app.get("/", function(req, res) {
     res.render("index");
+});
+
+app.post("/addUser", function(req, res) {
+    //console.log(req.body);
+
+    // Extract the details of the new user
+    let new_user = {};
+    new_user.usr_name = req.body.usr_name;
+    new_user.email = req.body.email;
+    new_user.password = req.body.password;
+
+    // Add the new user in the users list
+    users.push(new_user)
+    //console.log(users);
+
+
 });
 
 // Start the server
