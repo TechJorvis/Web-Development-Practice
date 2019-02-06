cf8fdae9273974a57ed84a9f104bedfd8661deb8
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-02-06 14:21:32 +0530

    Initialised app.js

---
+++ Practice/Profile-Practice/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const ip = require('ip');
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Connected!");
+});
+
+app.listen(3000, ip.address, function() {
+    console.log("Server started serving at: " + ip.address() + ":3000");
+});
