cb5ac13b0640f916e8389ee70e77414f744d26ec
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-02-11 23:07:10 +0530

    Defined the /results route

--- [20] Working with APIs/Movie-API/app.js
+++ [20] Working with APIs/Movie-API/app.js
@@ -7,6 +7,15 @@ app.get('/', function(req, res) {
     res.send("Welcome to the Movie API app hompage");
 });
 
+app.get("/results", function(req, res) {
+    request('http://www.omdbapi.com/?s=anime&apikey=thewdb', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            let results = JSON.parse(body);
+            res.send(results);
+        }
+    });
+});
+
 const port_nbr = 3000;
 app.listen(port_nbr, function() {
     console.log("Server is serving at port: " + port_nbr);
