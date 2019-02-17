27d75f13ba74ced6472aba5697058e559cd2a4c8
Ayush Singhal <ayush.singhal.as30@gmail.com>
2019-02-17 13:28:47 +0530

    app.js initialised

---
+++ [21] YelpCamp/v1/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", function(req, res) {
+    res.send("Site under Construction");
+});
+
+const port_nbr = 3000;
+app.listen(port_nbr, function() {
+    console.log("Server started serving at: " + port_nbr);
+});
